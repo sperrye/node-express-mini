6786f5253d30ec4cf48dca4da46b7065cda3d57e
Emily Sperry <sperrye@gmail.com>
2017-08-13 20:41:48 -0700

    completed mini

--- src/app.js
+++ src/app.js
@@ -10,10 +10,53 @@ server.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
-  const contents = fs.readFileSync('words.txt', 'utf8');
+  const contents = fs.readFileSync('../words.txt', 'utf8');
   return contents.split('\n');
 };
 
-// TODO: your code to handle requests
+const guessed = {};
+const words = readWords(); //contain array of words
+const index = Math.floor(Math.random() * words.length);
+//gets a random index
 
-server.listen(3000);
+const word = words[index];
+console.log(word);
+
+
+// let wordSoFar = word.replace(/([a-zA-Z])/g, '-');
+// console.log(wordSoFar);
+
+server.post('/guess', (req, res) => {
+  //request body req.body
+  const letter = req.body.letter;
+  if (!letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: "Must provide a letter."});
+    return;
+  }
+  if (letter.length !== 1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: "Must provide a single letter."});
+    return;
+  }
+  if (guessed[letter]) {
+    res.send(`${letter} already guessed.`);
+  }
+  if (word.split('').includes(req.body.letter)) {
+    guessed[letter] = true;
+    res.send(`word contains ${req.body.letter} !`);
+  }
+});
+
+server.get('/', (req, res) => {
+  const wordSoFarArray = Array.from(word).map((letter) => {
+    if (guessed[letter]) {
+      return letter;
+    }
+    return '-';
+  });
+  const wordSoFarString = wordSoFarArray.join('');
+  res.send({ wordSoFarString, guessed });
+});
+
+server.listen(8080);
